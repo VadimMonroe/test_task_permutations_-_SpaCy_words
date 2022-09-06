dd4d7016e65cda37e0dc7fedf5ec4f9664cee52f
Vadim <82668410+VadimMonroe@users.noreply.github.com>
2022-09-06 21:22:54 +0300

    Delete command.sh

--- command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 /Users/vadimmonroe/_PROGGING_2/_projects/28_test_task_junior/permute.py 3
-/Users/vadimmonroe/_PROGGING_2/_projects/28_test_task_junior/venv/bin/python3.10 /Users/vadimmonroe/_PROGGING_2/_projects/28_test_task_junior/find_words.py dd.txt hh.html
